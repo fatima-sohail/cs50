8e4f970527e700e4841012275c670806a7310706
unknown <fatimasohailhunzai@gmail.com>
2023-12-20 16:43:05 -0500

    added files to week-0-Scratch folder

---
+++ week-0-Scratch/hello.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    string first = get_string("What's your first name? ");
+    string last = get_string("What's your last name? ");
+
+    printf("hello, %s %s\n", first, last);
+    //printf(try last, last OR last, first)
+    //printf("Hello, world\n")
+}
